eff2a8902a546de9a5fb464888efdd157759d71e
Jehian <sendomoka2@gmail.com>
2024-07-26 21:10:27 +0700

    Remove unused RSS feed dependency and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://soedirmandigitalschool.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
