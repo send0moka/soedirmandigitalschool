48f1792816b774db02e0674794795cb54fd81d59
Jehian <166832786+send0moka@users.noreply.github.com>
2024-07-26 19:49:21 +0700

    TEST PROD (#1)
    * chore: Update site title and font styles
    
    * chore: Update site title, font styles, and dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        "ijo-tua": "#43594B",
+        "ijo-muda": "#DFDDAE",
+        "kuning": "#F9A402",
+        "oren": "#FB6500",
+        "bata": "#992800",
+        "hitam": "#252525",
+      },
       fontFamily: {
-        "sans": ["Atkinson", ...defaultTheme.fontFamily.sans],
+        "sans": ["Literata", ...defaultTheme.fontFamily.sans],
+        "display": ["Sugo", ...defaultTheme.fontFamily.sans],
       },
       typography: {
         DEFAULT: {
